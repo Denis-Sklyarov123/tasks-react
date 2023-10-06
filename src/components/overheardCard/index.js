e9e14fb6334748aea461f8d2aa558f879d493f78
Denis-Sklyarov123 <unrealgoodandyou@mail.ru>
2023-10-06 12:14:35 +0300

    card component

--- src/components/overheardCard/index.js
+++ src/components/overheardCard/index.js
@@ -1,21 +1,26 @@
 import styles from "./style.module.css";
+import { Avatar } from "@mui/material";
 
 function OverheardCard() {
   return (
-    <header className="App-header">
-      <div className="ImageContainer"></div>
-      <div className="Information">
-        <div className="Name">
-          <span className="Text">The Montcalm At Brewery London City</span>
-        </div>
-        <div className="City">Westminster Borough, London</div>
-        <div className="Grade">
-          <div className="Assessment">4.7</div>
-          <div className="Quality">Exceptional</div>
-          <div className="QuantityReviews">3014 reviews</div>
-        </div>
-        <div className="StartingPrice">
-          Starting from <span className="Price">US$68</span>
+    <header className={styles.appHeader}>
+      <div className={styles.nameHotel}>Hotel Equatorial Melaka</div>
+      <div className={styles.comment}>
+        "Great hotel. Everything is clean, cozy, tastefully done. Nice design,
+        new furniture, snow-white fluffy towels. The staff is polite and
+        friendly. There were no complaints about the cleaning. I was pleasantly
+        surprised by the free Wi-Fi with good speed and the availability of
+        parking. I will definitely come back to Ekipazh again."
+      </div>
+      <div>
+        <Avatar
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+          sx={{ width: 56, height: 56 }}
+        />
+        <div>
+          <div className={styles.name}>Annette Black</div>
+          <div className={styles.jobTitle}>UX / UI Designer</div>
         </div>
       </div>
     </header>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import CardHotel from "./components/cartHotel";
-import { Avatar } from "@mui/material";
 import OverheardCard from "./components/overheardCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CardHotel />
-    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
     <OverheardCard />
   </React.StrictMode>
 );
